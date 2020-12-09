4053548dc771784bf9fa5c1a357566dc3efb7fab
Alexsandr Chigarev <majestik13@mail.ru>
2020-12-09 19:11:08 +0300

    Fix 12

--- controllers/cards.js
+++ controllers/cards.js
@@ -6,9 +6,8 @@ const pathToCards = path.join(__dirname, '..', 'data', 'cards.json');
 
 function getCards(req, res) {
   return readFile(pathToCards)
-    .then((cards) =>{return res.status(200).send(cards)})
-    .catch((err) =>{return res.status(500).send({ status: 500, message: err.message })
-  });
+    .then((cards) => res.status(200).send(cards))
+    .catch((err) => res.status(500).send({ status: 500, message: err.message }));
 }
 
 module.exports = getCards;
--- controllers/users.js
+++ controllers/users.js
@@ -5,13 +5,13 @@ const pathToUser = path.join(__dirname, '..', 'data', 'users.json');
 
 function getUsers(req, res) {
   return readFile(pathToUser)
-    .then((users) =>{return res.status(200).send(users)})
-    .catch((err) =>{ return res.status(500).send({ status: 500, message: err.message })});
+    .then((users) => res.status(200).send(users))
+    .catch((err) => res.status(500).send({ status: 500, message: err.message }));
 }
 
 function getUsersId(req, res) {
   return readFile(pathToUser)
-    .then((users) =>{return users.find((user) => user._id === req.params.id)})
+    .then((users) => users.find((user) => user._id === req.params.id))
     .then((user) => {
       if (!user) {
         return res.status(404).send({ message: 'Нет пользователя с таким id' });
@@ -19,7 +19,7 @@ function getUsersId(req, res) {
 
       return res.status(200).send(user);
     })
-    .catch((err) =>{return res.status(500).send(err)});
+    .catch((err) => res.status(500).send(err));
 }
 
 module.exports = { getUsers, getUsersId };
